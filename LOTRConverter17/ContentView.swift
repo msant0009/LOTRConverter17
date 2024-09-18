01a82300251bf8015442a4d5e0d1ada1afb309ea
Mark Santoro <mark_santoro@comcast.net>
2024-09-17 19:38:25 -0700

    Initial setup

--- LOTRConverter17/ContentView.swift
+++ LOTRConverter17/ContentView.swift
@@ -9,13 +9,92 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            //Background image
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack{
+                //Prancing pony image
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                
+                //currency xchnge text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                
+                //currency conversion
+                
+                
+                
+                HStack{
+                    // left conversion section
+                    VStack{
+                        //currency
+                        HStack{
+                            //currency image
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            
+                            //currency text
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            
+                        }
+                        //text field
+                        Text("Text Field")
+                            .foregroundStyle(.red)
+                        
+                    }
+                    
+                    //equal sign
+                    Image(systemName: "equal")// from SF symbols library
+                        .font(.largeTitle)//SF symbols can be treated as text or as an image
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
+                    
+ 
+                    //right conversion section
+                    VStack{
+                        //currency
+                        HStack{
+                            //currency text
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            //currency image
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            
+                        }
+                        //text field
+                        Text("Text Field")
+                            .foregroundStyle(.red)
+                        
+                    }
+                }
+                
+                Spacer()
+                
+                //info button
+                Image(systemName: "info.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                
+            }
+       //     .border(.blue)// for debug to see outline of VStack in live mode
+            
+            
         }
-        .padding()
     }
 }
 
