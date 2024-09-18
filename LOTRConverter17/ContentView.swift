6455f7f88d2b81960e4447512af9182ff6ea4056
Mark Santoro <mark_santoro@comcast.net>
2024-09-17 18:31:58 -0700

    Initial Commit

---
+++ LOTRConverter17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LOTRConverter17
+//
+//  Created by Mark Santoro on 9/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
