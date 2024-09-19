c0c5bb54227c9b6a6fd7296625f11c3d4b114b01
Mark Santoro <mark_santoro@comcast.net>
2024-09-18 17:57:40 -0700

    Created and setup exchange info view

---
+++ LOTRConverter17/ExchangeInfo.swift
@@ -0,0 +1,73 @@
+//
+//  ExchangeInfo.swift
+//  LOTRConverter17
+//
+//  Created by Mark Santoro on 9/18/24.
+//
+
+import SwiftUI
+
+struct ExchangeInfo: View {
+    var body: some View {
+        ZStack{
+            //Background parchment
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            
+            VStack{
+                // title
+                Text("Exchange Rates")
+                    .font(.largeTitle)
+                    .tracking(3)
+                
+                //description
+                Text("Here at the Prancing Pony, we are happy to offer you a place where you can exchange all the known currencies in the entire world except one. We used to take Brandy Bucks, but after finding out that it was a person instead of a piece of paper, we realized it had no value to us. Below is a simple guide to our currency exchange rates:")
+                    .font(.title2)
+                    .padding()
+                
+                
+                //exchange rates
+                HStack{
+                    
+                    //left currency image
+                    
+                    Image(.goldpiece)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 33)
+
+                    //exchange rate text
+                    Text("1 Gold Piece = 4 Gold Pennies")
+                    
+                    
+                    //right currency image
+                    
+                    Image(.goldpenny)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 33)
+                    
+                }
+                
+                // done button
+                Button("Done"){
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.brown)
+                .font(.largeTitle)
+                .padding()
+                .foregroundStyle(.white)
+                
+            }
+            .foregroundStyle(.black)
+            
+        }
+    }
+}
+
+#Preview {
+    ExchangeInfo()
+}
