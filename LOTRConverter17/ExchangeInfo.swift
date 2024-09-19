9f32e52930354cc58ef37916903c63c9eee52564
Mark Santoro <mark_santoro@comcast.net>
2024-09-18 19:18:36 -0700

    Extract Hstack and use as a custom view, create instances to modify custom view

--- LOTRConverter17/ExchangeInfo.swift
+++ LOTRConverter17/ExchangeInfo.swift
@@ -29,27 +29,14 @@ struct ExchangeInfo: View {
                 
                 
                 //exchange rates
-                HStack{
-                    
-                    //left currency image
-                    
-                    Image(.goldpiece)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 33)
-
-                    //exchange rate text
-                    Text("1 Gold Piece = 4 Gold Pennies")
-                    
-                    
-                    //right currency image
-                    
-                    Image(.goldpenny)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 33)
-                    
-                }
+                // Four instances
+                ExchangeRate(leftImage: .goldpiece, text: "1 Gold Piece = 4 Gold Pennies", rightImage: .goldpenny)
+                
+                ExchangeRate(leftImage: .goldpenny, text: "1 Gold Penny = 4 Silver Pieces", rightImage: .silverpiece)
+               
+                ExchangeRate(leftImage: .silverpiece, text: "1 Silver Piece = 4 Silver Pennies", rightImage: .silverpenny)
+              
+                ExchangeRate(leftImage: .silverpenny, text: "1 Silver Penny = 100 Copper Pennies", rightImage: .copperpenny)
                 
                 // done button
                 Button("Done"){
@@ -71,3 +58,6 @@ struct ExchangeInfo: View {
 #Preview {
     ExchangeInfo()
 }
+
+
+
