9504fe79ec956fecd90bbf8add0dc3fdd9f939ca
Mark Santoro <mark_santoro@comcast.net>
2024-09-19 18:56:11 -0700

    create a currency model

---
+++ LOTRConverter17/Currency.swift
@@ -0,0 +1,17 @@
+//
+//  Currency.swift
+//  LOTRConverter17
+//
+//  Created by Mark Santoro on 9/19/24.
+//
+
+enum Currency: Double {
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+    
+}
+
+
